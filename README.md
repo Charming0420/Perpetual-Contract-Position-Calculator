0ca460e01c029eef6df95edc624323b6671c1c9a
余昌旻 Yu Chang-Min <tommy20326999@gmail.com>
2023-11-18 15:48:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Perpetual-Contract-Position-Calculator
+ This is a computer used to control the open position size of perpetual contracts.
---
+++ calculate.js
@@ -0,0 +1,67 @@
+// 確保 DOM 完全加載後再執行腳本
+document.addEventListener("DOMContentLoaded", (event) => {
+  var currentDirection = "long"; // 初始化變量
+
+  function selectDirection(direction) {
+    document.getElementById("long").classList.remove("selected");
+    document.getElementById("short").classList.remove("selected");
+    document.getElementById(direction).classList.add("selected");
+    currentDirection = direction;
+  }
+
+  // 定義計算結果的函數
+  function calculateResult(
+    direction,
+    positionSize,
+    openingPrice,
+    stopLossPrice,
+    maxLoss
+  ) {
+    positionSize = parseFloat(positionSize);
+    openingPrice = parseFloat(openingPrice);
+    stopLossPrice = parseFloat(stopLossPrice);
+    maxLoss = parseFloat(maxLoss);
+
+    var result = 0;
+
+    if (direction === "long") {
+      result =
+        maxLoss /
+        (((openingPrice - stopLossPrice) / openingPrice) * 100) /
+        (positionSize / 100);
+    } else if (direction === "short") {
+      result =
+        maxLoss /
+        -(((openingPrice - stopLossPrice) / openingPrice) * 100) /
+        (positionSize / 100);
+    }
+
+    return result;
+  }
+
+  // 為做多和做空按鈕添加點擊事件
+  document.getElementById("long").addEventListener("click", function () {
+    selectDirection("long");
+  });
+  document.getElementById("short").addEventListener("click", function () {
+    selectDirection("short");
+  });
+
+  // 為計算按鈕添加點擊事件
+  document.getElementById("confirm").addEventListener("click", function () {
+    const positionSize = document.getElementById("position-size").value;
+    const openingPrice = document.getElementById("opening-price").value;
+    const stopLossPrice = document.getElementById("stop-loss-price").value;
+    const maxLoss = document.getElementById("max-loss").value;
+
+    // 執行計算並更新結果
+    const result = calculateResult(
+      currentDirection,
+      positionSize,
+      openingPrice,
+      stopLossPrice,
+      maxLoss
+    );
+    document.getElementById("resultSpan").innerText = result.toFixed(2);
+  });
+});
